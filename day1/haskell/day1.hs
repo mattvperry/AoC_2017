ecdd96ccb9262582270d68ce36d39e71c8ac8e8f
Matt Perry <muffinman616@gmail.com>
2017-12-01 02:43:11 -0500

    Day 1 haskell

--- day1/haskell/day1.hs
+++ day1/haskell/day1.hs
@@ -1,3 +1,16 @@
+rotate :: Int -> [a] -> [a]
+rotate _ [] = []
+rotate n xs = zipWith const (drop n (cycle xs)) xs
+
+sumPairs :: Int -> [Int] -> Int
+sumPairs d xs = sum . zipWith fn (rotate d xs) $ xs
+    where fn a b = if a == b then a else 0
+
+solve :: Int -> String -> Int
+solve d = sumPairs d . map (read . pure)
+
+main :: IO ()
 main = do
     input <- readFile "day1/input.txt"
-    putStrLn input
+    print . solve 1 $ input
+    print . solve (length input `div` 2) $ input
