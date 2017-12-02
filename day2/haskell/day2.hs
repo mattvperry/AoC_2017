a281024dc1475b95c25de50e69c91e306169f5a4
Matt Perry <muffinman616@gmail.com>
2017-12-02 04:25:35 -0500

    cleanup

--- day2/haskell/day2.hs
+++ day2/haskell/day2.hs
@@ -1,30 +1,14 @@
-import Control.Arrow ((&&&))
-import Data.List (subsequences)
-import Data.List.Split (splitOn)
-import Data.Tuple (swap)
-
 readGrid :: String -> IO [[Int]]
-readGrid s = map (map read . splitOn "\t") . lines <$> readFile s
-
-seqPairs :: [a] -> [(a, a)]
-seqPairs = map fn . filter ((== 2) . length) . subsequences
-    where fn [a, b] = (a, b)
-
-allPairs :: [a] -> [(a, a)]
-allPairs xs = ps ++ map swap ps
-    where ps = seqPairs xs
-
-mapSum :: Num b => (a -> b) -> [a] -> b
-mapSum f = sum . map f
+readGrid s = map (map read . words) . lines <$> readFile s
 
 part1 :: [Int] -> Int
-part1 = uncurry subtract . (minimum &&& maximum)
+part1 = (-) <$> maximum <*> minimum
 
 part2 :: [Int] -> Int
-part2 = fst . head . filter ((== 0) . snd) . map (uncurry divMod) . allPairs
+part2 xs = head [d | (d, 0) <- divMod <$> xs <*> xs, d /= 1]
 
 main :: IO ()
 main = do
     grid <- readGrid "day2/input.txt"
-    print . mapSum part1 $ grid
-    print . mapSum part2 $ grid
+    print . sum . map part1 $ grid
+    print . sum . map part2 $ grid
