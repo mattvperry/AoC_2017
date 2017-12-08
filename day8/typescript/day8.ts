7b3926d81d23e4da23d742be28db314d645ba021
Matt Perry <muffinman616@gmail.com>
2017-12-08 00:23:12 -0500

    Day 8

---
+++ day8/typescript/day8.ts
@@ -0,0 +1,52 @@
+import { readFile } from 'fs';
+import { promisify } from 'util';
+
+import * as R from 'ramda';
+
+interface Registers {
+    [name: string]: number;
+}
+
+interface Instruction {
+    register: string;
+    amount: number;
+    condition: string[];
+}
+
+const getRegister = (regs: Registers, reg: string) => R.compose<Registers, number, number>(
+    R.defaultTo(0),
+    R.prop(reg),
+)(regs);
+
+const parse = (ins: string) => {
+    const [register, op, num, _, ...condition] = R.split(' ', ins);
+    const amount = parseInt(num, 10);
+    return {
+        register,
+        amount: op === 'inc' ? amount : -1 * amount,
+        condition,
+    };
+};
+
+const solve = (instructions: Instruction[]): [number, Registers] => {
+    let max = 0;
+    const regs: Registers = {};
+    for (const { register, amount, condition } of instructions) {
+        const [cond, op, val] = condition;
+        if (eval(`${getRegister(regs, cond)} ${op} ${val}`)) {
+            regs[register] = getRegister(regs, register) + amount;
+            max = R.max(max, regs[register]);
+        }
+    }
+
+    return [max, regs];
+};
+
+(async () => {
+    const input = await promisify(readFile)('day8/input.txt', 'utf8');
+    const instructions = R.map(parse, R.split('\r\n', input));
+    const [max, regs] = solve(instructions);
+
+    console.log(Math.max(...R.values(regs)));
+    console.log(max);
+})();
