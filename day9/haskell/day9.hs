824cfdb43369e9558000ee956bbc59a3b787c438
Matt Perry <muffinman616@gmail.com>
2017-12-09 03:15:17 -0500

    Day 9 haskell

---
+++ day9/haskell/day9.hs
@@ -0,0 +1,18 @@
+solve :: Bool -> Int -> Int -> Int -> String -> (Int, Int)
+solve _ _ s n []        = (s, n)
+solve g d s n (c:xs)
+    | c == '!'          = solve g d s n (tail xs)
+    | c == '<' && g     = solve g d s (n + 1) xs
+    | c == '<' && not g = solve True d s n xs
+    | c == '>'          = solve False d s n xs
+    | c == '{' && g     = solve g d s (n + 1) xs
+    | c == '{' && not g = solve g (d + 1) (s + d + 1) n xs
+    | c == '}' && g     = solve g d s (n + 1) xs
+    | c == '}' && not g = solve g (d - 1) s n xs
+    | g                 = solve g d s (n + 1) xs
+    | otherwise         = solve g d s n xs
+
+main :: IO ()
+main = do
+    input <- readFile "day9/input.txt"
+    print . solve False 0 0 0 $ input
