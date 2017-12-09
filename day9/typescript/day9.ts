27478bf00a74ac8bcc246e2708c3f709d8a7552c
Matt Perry <muffinman616@gmail.com>
2017-12-09 00:24:25 -0500

    Day9 TS

--- day9/typescript/day9.ts
+++ day9/typescript/day9.ts
@@ -3,17 +3,43 @@ import { promisify } from 'util';
 
 import * as R from 'ramda';
 
-const part1 = () => {
-    return '';
-}
+const solve = (chars: string[]) => {
+    let garbage = false;
+    let depth = 0;
+    let sum = 0;
+    let count = 0;
+    while (chars.length > 0) {
+        const c = chars.pop();
+        switch(c) {
+            case '!':
+                chars.pop();
+                break;
+            case '<':
+                garbage ? count++ : garbage = true;
+                break;
+            case '>':
+                garbage = false;
+                break;
+            case '{':
+                garbage ? count++ : sum += ++depth;
+                break;
+            case '}':
+                garbage ? count++ : depth--;
+                break;
+            default:
+                if (garbage) count++;
+                break;
+        }
+    }
 
-const part2 = () => {
-    return '';
+    return [sum, count];
 }
 
 (async () => {
     const input = await promisify(readFile)('day9/input.txt', 'utf8');
+    const chars = Array.from(input).reverse();
 
-    console.log(part1());
-    console.log(part2());
+    const [part1, part2] = solve(chars);
+    console.log(part1);
+    console.log(part2);
 })();
