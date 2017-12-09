443f982db96e4ab5563008002ee87be286301708
Matt Perry <muffinman616@gmail.com>
2017-12-09 15:37:44 -0500

    day 8 haskell

--- day8/haskell/day8.hs
+++ day8/haskell/day8.hs
@@ -1,17 +1,33 @@
-type Instruction = (String, Int, String, Int -> Int -> Bool, Int)
+import Control.Arrow (first)
+import Data.Map (Map, findWithDefault, empty, insert)
+
+type State = (Map String Int, Int)
+type Instruction = (String, Int, String, Int -> Bool)
 
 op :: String -> (Int -> Int -> Bool)
-op ">"  = (>)
-op "<"  = (<)
-op ">=" = (>=)
-op "<=" = (<=)
+op ">"  = (<)
+op "<"  = (>)
+op ">=" = (<=)
+op "<=" = (>=)
 op "==" = (==)
 op "!=" = (/=)
+op _    = error "Unknown operation"
 
 parse :: String -> Instruction
-parse s = (r, if m == "int" then read m else -(read m), r', op o, read v)
+parse s = (r, if m == "inc" then read n else -(read n), r', op o (read v))
     where [r, m, n, "if", r', o, v] = words s
+
+modify :: State -> Instruction -> State
+modify s@(rs, m) (r, a, r', c)
+        | not . cond $ rs   = s
+        | otherwise         = (insert r val rs, max m val)
+    where cond = c . findWithDefault 0 r'
+          val  = findWithDefault 0 r rs + a
+
+solve :: [Instruction] -> (Int, Int)
+solve = first maximum . foldl modify (empty, 0)
 
 main :: IO ()
 main = do
     is <- map parse . lines <$> readFile "day8/input.txt"
+    print . solve $ is
