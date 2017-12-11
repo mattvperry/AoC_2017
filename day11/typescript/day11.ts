0492b554a45154ade8aa98484dcd9f4a62407719
Matt Perry <muffinman616@gmail.com>
2017-12-11 00:23:24 -0500

    Day11 ts

--- day11/typescript/day11.ts
+++ day11/typescript/day11.ts
@@ -3,17 +3,38 @@ import { promisify } from 'util';
 
 import * as R from 'ramda';
 
-const part1 = (input: string) => {
-    return input;
+type Coord = [number, number, number];
+type Moves = { [dir: string]: (coord: Coord) => Coord };
+
+const moves: Moves = {
+    n: ([x, y, z]) => [x - 1, y + 1, z],
+    ne: ([x, y, z]) => [x, y + 1, z - 1],
+    nw: ([x, y, z]) => [x - 1, y, z + 1],
+    s: ([x, y, z]) => [x + 1, y - 1, z],
+    se: ([x, y, z]) => [x + 1, y, z - 1],
+    sw: ([x, y, z]) => [x, y - 1, z + 1],
 };
 
-const part2 = (input: string) => {
-    return input;
+const path = (dirs: string[]) => R.scan<string, Coord>(
+    (acc, curr) => moves[curr](acc),
+    [0, 0, 0],
+    dirs,
+);
+
+const part1 = (dirs: string[]) => {
+    const end = R.last(path(dirs))!;
+    return Math.max(...end.map(Math.abs));
+};
+
+const part2 = (dirs: string[]) => {
+    const points = path(dirs);
+    return Math.max(...R.map(p => Math.max(...p.map(Math.abs)), points));
 };
 
 (async () => {
     const input = await promisify(readFile)('day11/input.txt', 'utf8');
+    const dirs = input.split(',');
 
-    console.log(part1(input));
-    console.log(part2(input));
+    console.log(part1(dirs));
+    console.log(part2(dirs));
 })();
