6c3dcf19cd8cff1eb680ddb51decf183f27c4fe6
Matt Perry <muffinman616@gmail.com>
2017-12-13 00:47:53 -0500

    Day13 brute force

--- day13/typescript/day13.ts
+++ day13/typescript/day13.ts
@@ -3,17 +3,63 @@ import { promisify } from 'util';
 
 import * as R from 'ramda';
 
-const part1 = () => {
-    return '';
-};
+interface Layer {
+    range: number;
+    scanner: number;
+}
 
-const part2 = () => {
-    return '';
+type Firewall = { [depth: number]: Layer };
+
+const parse = R.compose<string[], Firewall[], Firewall>(
+    R.mergeAll,
+    R.map(
+        R.compose<string, string[], number[], Firewall>(
+            ([d, r]) => ({ [d]: { range: r, scanner: 0 } }),
+            R.map(parseInt),
+            R.split(': '),
+        ),
+    ),
+);
+
+const step = R.mapObjIndexed(
+    ({scanner, range}, key, obj) => ({
+        range,
+        scanner: (scanner + 1) % (range * 2 - 2)
+    }),
+);
+
+function* simulate(firewall: Firewall): IterableIterator<[number, number]> {
+    const max = Math.max(...R.map(parseInt, R.keys(firewall)));
+    for (const p of R.range(0, max + 1)) {
+        const layer = firewall[p];
+        if (layer !== undefined && layer.scanner === 0) {
+            yield [p, layer.range];
+        }
+
+        firewall = step(firewall);
+    }
+}
+
+const part1 = (firewall: Firewall) => R.reduce(
+    (acc, [depth, range]) => acc + depth * range,
+    0,
+    Array.from(simulate(firewall)),
+);
+
+const part2 = (firewall: Firewall) => {
+    for (let i = 0;; ++i) {
+        if (Array.from(simulate(firewall)).length === 0) {
+            return i;
+        }
+
+        firewall = step(firewall);
+    }
 };
 
 (async () => {
     const input = await promisify(readFile)('day13/input.txt', 'utf8');
+    const firewall = parse(R.split('\r\n', input));
 
-    console.log(part1());
-    console.log(part2());
+    console.log(part1(firewall));
+    console.log(part2(firewall));
 })();
