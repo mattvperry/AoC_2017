9fb0d7f68c08fcbeb1975917a08856b3cf409d9e
Matt Perry <muffinman616@gmail.com>
2017-12-15 02:17:56 -0500

    Cleanup

--- day15/haskell/day15.hs
+++ day15/haskell/day15.hs
@@ -6,23 +6,19 @@ generate :: Int -> Int -> (Int -> Bool) -> [Int]
 generate v f c = map (.&. 0xFFFF) . filter c . iterate next $ v
     where next = flip mod 2147483647 . (* f)
 
-genA :: Generator
-genA = generate 883 16807
-
-genB :: Generator
-genB = generate 879 48271
-
 solve :: [Int] -> [Int] -> Int -> Int
 solve a b n = length . filter id . take n $ zipWith (==) a b
 
-part1 :: Int
-part1 = solve (genA $ const True) (genB $ const True) 40000000
+part1 :: Generator -> Generator -> Int
+part1 a b = solve (a $ const True) (b $ const True) 40000000
 
-part2 :: Int
-part2 = solve (genA $ f 4) (genB $ f 8) 5000000
+part2 :: Generator -> Generator -> Int
+part2 a b = solve (a $ f 4) (b $ f 8) 5000000
     where f c = (== 0) . flip mod c
 
 main :: IO ()
 main = do
-    print part1
-    print part2
+    let a = generate 883 16807
+    let b = generate 879 48271
+    print $ part1 a b
+    print $ part2 a b
