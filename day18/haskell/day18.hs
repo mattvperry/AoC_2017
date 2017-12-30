8da6b5a90c960bd2f7d87e6f9d4e0937b6408bce
Matt Perry <muffinman616@gmail.com>
2017-12-30 17:59:30 -0500

    Day18 monadic

--- day18/haskell/day18.hs
+++ day18/haskell/day18.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
 
-import Data.Char (isLetter)
+import Data.Char (isAlpha)
 import Data.Map (Map, empty)
 import Data.Maybe (mapMaybe)
-import qualified Data.Sequence as S
+import qualified Data.Vector as V
 import Control.Lens
-import Control.Monad (unless)
-import Control.Monad.State (State, modify, gets, runState)
+import Control.Monad (unless, when)
+import Control.Monad.State (State, execState)
 
 type Param = Either Char Int
 type Registers = Map Char Int
@@ -15,12 +17,9 @@ type Registers = Map Char Int
 data Op
     = Snd Param
     | Set Char Param
-    | Add Char Param
-    | Mul Char Param
-    | Mod Char Param
+    | Bin (Int -> Int -> Int) Char Param
     | Rcv Param
     | Jgz Param Param
-      deriving (Eq, Show)
 
 data Status 
     = Terminated
@@ -33,56 +32,46 @@ data Program = Program
     , _regs   :: Registers
     , _sounds :: [Int]
     , _status :: Status
-    , _code   :: S.Seq Op
-    } deriving (Show)
+    , _code   :: V.Vector Op
+    }
 makeLenses ''Program
 
-parse :: String -> S.Seq Op
-parse = S.fromList . mapMaybe (go . words) . lines
-    where go ["snd", x]    = Just $ Snd (f x)
-          go ["set", x, y] = Just $ Set (head x) (f y)
-          go ["add", x, y] = Just $ Add (head x) (f y)
-          go ["mul", x, y] = Just $ Mul (head x) (f y)
-          go ["mod", x, y] = Just $ Mod (head x) (f y)
-          go ["rcv", x]    = Just $ Rcv (f x)
-          go ["jgz", x, y] = Just $ Jgz (f x) (f y)
-          go _             = Nothing
-          f x
-            | all isLetter x = Left $ head x
-            | otherwise      = Right $ read x
+parse :: String -> V.Vector Op
+parse = V.fromList . mapMaybe (go . words) . lines
+    where go ["snd", x]     = Just $ Snd (f x)
+          go ["set", x, y]  = Just $ Set (head x) (f y)
+          go ["add", x, y]  = Just $ Bin (+) (head x) (f y)
+          go ["mul", x, y]  = Just $ Bin (*) (head x) (f y)
+          go ["mod", x, y]  = Just $ Bin mod (head x) (f y)
+          go ["rcv", x]     = Just $ Rcv (f x)
+          go ["jgz", x, y]  = Just $ Jgz (f x) (f y)
+          go _              = Nothing
+          f [c] | isAlpha c = Left c
+          f str             = Right $ read str
 
 reg :: Char -> Lens' Program Int
 reg r = regs . at r . non 0
 
-val :: Param -> Program -> Int
-val = either (view . reg) const
+val :: Param -> State Program Int
+val = either (use . reg) pure
 
-perform :: Program -> Op -> Program
-perform p (Snd x)   = p & sounds %~ ((val x p) <|)
-perform p (Set x y) = p & reg x .~ (val y p)
-perform p (Add x y) = p & reg x +~ (val y p)
-perform p (Mul x y) = p & reg x *~ (val y p)
-perform p (Mod x y) = p & reg x %~ (`mod` (val y p))
-perform p (Rcv x)
-    | val x p /= 0 = p & status .~ Terminated 
-    | otherwise    = p
-perform p (Jgz x y)
-    | val x p > 0  = p & pc +~ (val y p - 1)
-    | otherwise    = p
+perform :: Op -> State Program ()
+perform (Snd x)     = (<|)   <$> val x >>= modifying sounds
+perform (Set x y)   =            val y >>= assign (reg x)
+perform (Bin f x y) = flip f <$> val y >>= modifying (reg x)
+perform (Rcv x)     = (/= 0) <$> val x >>= flip when (status .= Terminated)
+perform (Jgz x y)   = (>  0) <$> val x >>= flip when (modifying pc =<< ((+) . pred <$> val y))
 
-step :: Program -> Program
-step p = go $ p ^? code . ix (p ^. pc)
-    where go (Just o) = (perform p o) & pc +~ 1
-          go Nothing  = p & status .~ Terminated
+step :: State Program ()
+step = use pc >>= (\n -> preuse (code . ix n)) >>= \case
+    Just x  -> perform x >> pc += 1
+    Nothing -> status .= Terminated
 
 exec :: State Program ()
-exec = do
-    modify step
-    s <- gets (view status)
-    unless (s == Terminated) exec
+exec = step >> use status >>= (\s -> unless (s == Terminated) exec)
 
-part1 :: S.Seq Op -> Maybe Int
-part1 c = (snd $ runState exec p) ^? sounds . ix 0
+part1 :: V.Vector Op -> Maybe Int
+part1 c = execState exec p ^? sounds . ix 0
     where p = Program { _pc = 0, _regs = empty, _sounds = [], _status = Running, _code = c }
 
 main :: IO ()
